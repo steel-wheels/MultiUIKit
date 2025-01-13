4807c12c814a214f41ef198269d32ad4d63b8eb2
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2025-01-13 23:36:49 +0900

    test: Initial commit for TabView_macOS

---
+++ Tests/TabView_macOS/ViewController2.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TabView_macOS
+//
+//  Created by Tomoo Hamada on 2025/01/13.
+//
+
+import MultiUIKit
+import Cocoa
+
+class ViewController2: MIViewController
+{
+        @IBOutlet weak var mRootView: MIStack!
+        
+        override func viewDidLoad() {
+                super.viewDidLoad()
+
+                // Do any additional setup after loading the view.
+                let label = MILabel()
+                label.title = "Controller2"
+                mRootView.addArrangedSubView(label)
+        }
+
+        override var representedObject: Any? {
+                didSet {
+                // Update the view, if already loaded.
+                }
+        }
+
+
+}
+
