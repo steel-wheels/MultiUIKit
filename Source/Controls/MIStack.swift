c3b567af889ac1662693ba0a6751bd3ce144d42c
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2025-09-14 23:11:51 +0900

    MIStack: Support drop operations

--- Source/Controls/MIStack.swift
+++ Source/Controls/MIStack.swift
@@ -51,6 +51,118 @@ open class MIStack: MIInterfaceView
         public func removeAllSubviews() {
                 coreStackView().removeAllSubviews()
         }
+
+        /*
+         * support drop operation
+         */
+        public typealias DroppedCallback = (_ stack: MIStack, _ point: CGPoint, _ symbol: MISymbol) -> Void
+
+        private var mDroppedCallback: DroppedCallback? = nil
+
+        public func set(droppedCallback cbfunc: @escaping DroppedCallback) {
+                mDroppedCallback = cbfunc
+                #if os(OSX)
+                registerForDraggedTypes(Array(acceptableTypes))
+                #endif // os(OSX)
+        }
+
+#if os(OSX)
+        private var acceptableTypes: Set<NSPasteboard.PasteboardType> { return [ .fileURL, .png ] }
+
+        private let filteringOptions = [ NSPasteboard.ReadingOptionKey.urlReadingContentsConformToTypes:NSImage.imageTypes
+        ]
+
+        private func shouldAllowDrag(_ draggingInfo: NSDraggingInfo) -> Bool {
+                guard let _ = mDroppedCallback else {
+                        return false
+                }
+
+                let canAccept: Bool
+                let pasteBoard = draggingInfo.draggingPasteboard
+                if pasteBoard.canReadObject(forClasses: [NSImage.self], options: filteringOptions) {
+                        canAccept = true
+                } else {
+                        canAccept = false
+                }
+                //NSLog("shouldAllowDrag: \(canAccept)")
+                return canAccept
+        }
+
+        public var isReceivingDrag = false {
+                didSet {
+                        needsDisplay = true
+                }
+        }
+
+        public override func prepareForDragOperation(_ sender: NSDraggingInfo) -> Bool {
+                let allow = shouldAllowDrag(sender)
+                return allow
+        }
+
+        public override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+                let allow = shouldAllowDrag(sender)
+                isReceivingDrag = allow
+                return allow ? .copy : NSDragOperation()
+        }
+
+        public override func draggingExited(_ sender: NSDraggingInfo?) {
+                isReceivingDrag = false
+        }
+
+        public override func draw(_ dirtyRect: NSRect) {
+                super.draw(dirtyRect)
+                if isReceivingDrag {
+                        NSColor.selectedControlColor.set()
+
+                        let path = NSBezierPath(rect:bounds)
+                        path.lineWidth = 2.0 // Appearance.lineWidth
+                        path.stroke()
+                }
+        }
+
+        public override func performDragOperation(_ draggingInfo: NSDraggingInfo) -> Bool {
+                isReceivingDrag = false
+                let pasteBoard = draggingInfo.draggingPasteboard
+                let point = convert(draggingInfo.draggingLocation, from: nil)
+
+                //NSLog("performDragOperation: start")
+
+                guard let cbfunc = mDroppedCallback else {
+                        NSLog("[Error] No callback function at \(#file)")
+                        return false
+                }
+
+                guard let pasteboardItems = pasteBoard.pasteboardItems else {
+                        NSLog("[Error] No pasteboard item at \(#file)")
+                        return false
+                }
+
+                var result = false
+                //NSLog("performDragOperation: item num: \(pasteboardItems.count)")
+                for pasteboarditem in pasteboardItems {
+                        for type in pasteboarditem.types {
+                                switch type {
+                                case .png:
+                                        break // ignore
+                                case .fileURL, .URL:
+                                        if let data = pasteboarditem.data(forType: type) {
+                                                let url = NSURL(dataRepresentation: data, relativeTo: nil) as URL
+                                                NSLog("performDragOperation: (url) get data: \(url.path)")
+                                                if let sym = MISymbol.decode(fromURL: url) {
+                                                        cbfunc(self, point, sym)
+                                                        result = true
+                                                } else {
+                                                        NSLog("[Error] Failed to decode symbol at \(#file)")
+                                                }
+                                        }
+                                default:
+                                        NSLog("[Error] performDragOperation: Others")
+                                }
+                        }
+                }
+                return result
+        }
+#endif
 }
 
 
--- Tests/CollectionView_Test/ViewController.swift
+++ Tests/CollectionView_Test/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: NSViewController
 {
 
         @IBOutlet weak var mCollectionView: MICollectionView!
-        @IBOutlet weak var mDropView: MIDropView!
+        @IBOutlet weak var mStackView: MIStack!
         
         override func viewDidLoad() {
                 super.viewDidLoad()
@@ -23,6 +23,11 @@ class ViewController: NSViewController
                         .buttonHorizontalTopPress
                 ]
                 mCollectionView.set(symbols: symbols, size: .regular)
+
+                mStackView.set(droppedCallback: {
+                        (_ stack: MIStack, _ point: CGPoint, _ symbol: MISymbol) -> Void in
+                        NSLog("dropped symbol \(symbol.name) at (\(point.x), \(point.y))")
+                })
         }
 
         override var representedObject: Any? {
